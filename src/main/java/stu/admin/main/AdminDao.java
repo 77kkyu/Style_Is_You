878f06979da47a0e54510a417cbc8b90c64e6576
redson7 <redson7@daum.net>
2020-06-03 16:24:02 +0900

    쿼리수정

--- src/main/java/stu/admin/main/AdminDao.java
+++ src/main/java/stu/admin/main/AdminDao.java
@@ -5,21 +5,24 @@ import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
+import stu.common.common.CommandMap;
 import stu.common.dao.AbstractDao;
 
 @Repository("adminDao")
-public class AdminDao extends AbstractDao{
+public class AdminDao extends AbstractDao{ 
+	// 로그값을 받기 위해 AbstractDao를 상속한다 AbstractDao에서 MyBatis와 로그, 커넥션을 가져와 처리한다
+	// 굳이 따로 빼서 처리가 필요없다면 adminDao에서 커넥션을 받아와 바로 처리한다
 	
 	@SuppressWarnings("unchecked")
-	public List<Map<String,Object>> dashBoard(Map<String,Object> map) throws Exception { //adminMain대쉬보드 
+	public List<Map<String,Object>> dashBoard(CommandMap map) throws Exception { //adminMain대쉬보드 
 		
 		return (List<Map<String,Object>>) dashBoard("admin.dash_count",map);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public List<Map<String,Object>> order_admin_a(Map<String,Object> map) throws Exception { //adminMain대쉬보드 
+	public List<Map<String,Object>> order_admin_a(CommandMap map) throws Exception { //adminMain대쉬보드 
 		
-		return (List<Map<String,Object>>) order_admin_a("admin.order_admin_a",map);
+		return (List<Map<String,Object>>) order_admin_a("admin.order_admin_a",map.getMap());
 	}
 
 	
--- src/main/java/stu/admin/main/AdminMainService.java
+++ src/main/java/stu/admin/main/AdminMainService.java
@@ -3,10 +3,12 @@ package stu.admin.main;
 import java.util.List;
 import java.util.Map;
 
+import stu.common.common.CommandMap;
+
 public interface AdminMainService {
 	
-	List<Map<String, Object>> dashBoard(Map<String, Object> map) throws Exception; // AdminMain대쉬보드
+	List<Map<String, Object>> dashBoard(CommandMap commandMap) throws Exception; // AdminMain대쉬보드
 	
-	List<Map<String, Object>> order_admin_a(Map<String, Object> map) throws Exception; // 주문/배송-신규주문건
+	List<Map<String, Object>> order_admin_a(CommandMap map) throws Exception; // 주문/배송-신규주문건
 
 }
